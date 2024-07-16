9766238acff7d6eba3ac61db8d5d70aa3e60000d
Bhanu Partap Singh Rana <bhanurana3826@gmail.com>
2024-07-16 23:11:57 +0530

    Updated Navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -12,16 +12,16 @@ export default function Navbar() {
   return (
     <div className="navbar">
       <div className="logo">
-        <img src="Images/1_Logo.png" alt="logo" />
+        <Link to='/'><img src="Images/1_Logo.png" alt="logo" /></Link>
       </div>
 
       <ul className={`nav-links ${isMobileMenuOpen ? 'active' : ''}`}>
-        <li><Link to='' className='links'>Home</Link></li>
+        <li><Link to='/' className='links'>Home</Link></li>
         <li><Link to='' className='links'>About</Link></li>
         <li><Link to='' className='links'>Categories</Link></li>
         <li><Link to='' className='links'>Login/Register</Link></li>
         <li><Link to='' className='links'>Cart</Link></li>
-        <img id='cart' src="Images/cart.png" alt="cart" />
+        <Link to=''><img id='cart' src="Images/cart.png" alt="cart" /></Link>
       </ul>
 
       <div className="hamburger-icon" onClick={toggleMobileMenu}>
